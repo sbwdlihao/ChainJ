bd14204fc72b9cafc7dc23f5395ce2244eec1125
sbwdlihao <sbwdlihao@gmail.com>
2016-12-02 13:54:34 +0800

    add log4j2 configuration

--- src/main/java/com/lihao/crypto/ed25519/chainkd/Util.java
+++ src/main/java/com/lihao/crypto/ed25519/chainkd/Util.java
@@ -1,6 +1,8 @@
 package com.lihao.crypto.ed25519.chainkd;
 
 import com.lihao.encoding.VarInt;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,89 +16,91 @@ import java.util.Random;
  */
 public class Util {
 
-  public static XPrv newXPrv(InputStream io) throws IOException {
-    byte[] entropy = new byte[32];
-    if (io.read(entropy) != entropy.length) {
-      throw new IOException("read eof of io, but entropy not full");
-    }
-    return newXPrv(entropy);
-  }
+    private static Logger logger = LogManager.getLogger();
 
-  // if entropy is null, random bytes is used
-  public static XPrv newXPrv(byte[] entropy) {
-    if (entropy == null) {
-      entropy = new byte[32];
-      Random random = new Random();
-      random.setSeed(System.currentTimeMillis());
-      random.nextBytes(entropy);
+    public static XPrv newXPrv(InputStream io) throws IOException {
+        byte[] entropy = new byte[32];
+        if (io.read(entropy) != entropy.length) {
+            throw new IOException("read eof of io, but entropy not full");
+        }
+        return newXPrv(entropy);
     }
-    if (entropy.length != 32) {
-      throw new IllegalArgumentException("entropy length should be 32");
+
+    // if entropy is null, random bytes is used
+    public static XPrv newXPrv(byte[] entropy) {
+        if (entropy == null) {
+            entropy = new byte[32];
+            Random random = new Random();
+            random.setSeed(System.currentTimeMillis());
+            random.nextBytes(entropy);
+        }
+        if (entropy.length != 32) {
+            throw new IllegalArgumentException("entropy length should be 32");
+        }
+        MessageDigest md;
+        try {
+            md = MessageDigest.getInstance("SHA-512");
+        } catch (NoSuchAlgorithmException e) {
+            logger.catching(e);
+            return null;
+        }
+        md.update("Chain seed".getBytes());
+        md.update(entropy);
+        byte[] digest = md.digest();
+        modifyScalar(digest);
+        return new XPrv(digest);
     }
-    MessageDigest md;
-    try {
-      md = MessageDigest.getInstance("SHA-512");
-    } catch (NoSuchAlgorithmException e) {
-      e.printStackTrace();
-      return null;
+
+    public static PublicKey[] xPubKeys(XPub[] xpubs) {
+        PublicKey[] res = new PublicKey[xpubs.length];
+        for (int i = 0; i < xpubs.length; i++) {
+            res[i] = xpubs[i].publicKey();
+        }
+        return res;
     }
-    md.update("Chain seed".getBytes());
-    md.update(entropy);
-    byte[] digest = md.digest();
-    modifyScalar(digest);
-    return new XPrv(digest);
-  }
 
-  public static PublicKey[] xPubKeys(XPub[] xpubs) {
-    PublicKey[] res = new PublicKey[xpubs.length];
-    for (int i = 0; i < xpubs.length; i++) {
-      res[i] = xpubs[i].publicKey();
+    public static XPub[] deriveXPubs(XPub[] xpubs, byte[][] path) {
+        XPub[] res = new XPub[xpubs.length];
+        for (int i = 0; i < xpubs.length; i++) {
+            res[i] = xpubs[i].derive(path);
+        }
+        return res;
     }
-    return res;
-  }
 
-  public static XPub[] deriveXPubs(XPub[] xpubs, byte[][] path) {
-    XPub[] res = new XPub[xpubs.length];
-    for (int i = 0; i < xpubs.length; i++) {
-      res[i] = xpubs[i].derive(path);
+    static byte[] hashKeySaltSelector(byte version, byte[] key, byte[] salt, byte[] sel) {
+        MessageDigest helper = hashKeySaltHelper(version, key, salt);
+        byte[] l = new byte[10];
+        int n = VarInt.putUvarint(l, sel.length);
+        helper.update(l, 0, n);
+        helper.update(sel);
+        byte[] out = helper.digest();
+        modifyScalar(out);
+        return out;
     }
-    return res;
-  }
 
-  static byte[] hashKeySaltSelector(byte version, byte[] key, byte[] salt, byte[] sel) {
-    MessageDigest helper = hashKeySaltHelper(version, key, salt);
-    byte[] l = new byte[10];
-    int n = VarInt.putUvarint(l, sel.length);
-    helper.update(l, 0, n);
-    helper.update(sel);
-    byte[] out = helper.digest();
-    modifyScalar(out);
-    return out;
-  }
-
-  static byte[] hashKeySalt(byte version, byte[] key, byte[] salt) {
-    MessageDigest helper = hashKeySaltHelper(version, key, salt);
-    return helper.digest();
-  }
-
-  private static MessageDigest hashKeySaltHelper(byte version, byte[] key, byte[] salt) {
-    MessageDigest md;
-    try {
-      md = MessageDigest.getInstance("SHA-512");
-    } catch (NoSuchAlgorithmException e) {
-      e.printStackTrace();
-      return null;
+    static byte[] hashKeySalt(byte version, byte[] key, byte[] salt) {
+        MessageDigest helper = hashKeySaltHelper(version, key, salt);
+        return helper.digest();
     }
-    md.update(version);
-    md.update(key);
-    md.update(salt);
-    return md;
-  }
 
-  private static void modifyScalar(byte[] digest) {
-    digest[0] &= 248;
-    digest[31] &= 127;
-    digest[31] |= 64;
-  }
+    private static MessageDigest hashKeySaltHelper(byte version, byte[] key, byte[] salt) {
+        MessageDigest md;
+        try {
+            md = MessageDigest.getInstance("SHA-512");
+        } catch (NoSuchAlgorithmException e) {
+            logger.catching(e);
+            return null;
+        }
+        md.update(version);
+        md.update(key);
+        md.update(salt);
+        return md;
+    }
+
+    private static void modifyScalar(byte[] digest) {
+        digest[0] &= 248;
+        digest[31] &= 127;
+        digest[31] |= 64;
+    }
 
 }
--- src/test/java/com/lihao/crypto/Sha3Test.java
+++ src/test/java/com/lihao/crypto/Sha3Test.java
@@ -12,16 +12,16 @@ import java.security.Security;
  * Created by sbwdlihao on 11/28/16.
  */
 public class Sha3Test {
-  @BeforeClass
-  public static void before() {
-    Security.addProvider(new BouncyCastleProvider());
-  }
+    @BeforeClass
+    public static void before() {
+        Security.addProvider(new BouncyCastleProvider());
+    }
 
-  @Test
-  public void testSum256() throws NoSuchAlgorithmException {
-    byte[] data = {0,1,2};
-    byte[] hash = Sha3.Sum256(data);
-    byte[] target = {17,-122,-44,-102,74,-42,32,97,-113,118,15,41,-38,44,89,59,46,-62,-52,44,-19,105,-36,22,-127,115,-112,-40,97,-26,34,83,};
-    Assert.assertArrayEquals(hash, target);
-  }
+    @Test
+    public void testSum256() throws NoSuchAlgorithmException {
+        byte[] data = {0, 1, 2};
+        byte[] hash = Sha3.Sum256(data);
+        byte[] target = {17, -122, -44, -102, 74, -42, 32, 97, -113, 118, 15, 41, -38, 44, 89, 59, 46, -62, -52, 44, -19, 105, -36, 22, -127, 115, -112, -40, 97, -26, 34, 83,};
+        Assert.assertArrayEquals(hash, target);
+    }
 }
